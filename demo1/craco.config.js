4fccee05e3ebda09e0e5a7f2aa4c8796915b4543
牛巧玲 <qiaoling.niu@realai.ai>
2021-10-19 18:29:22 +0800

    fix: add lint check

---
+++ demo1/craco.config.js
@@ -0,0 +1,85 @@
+const CracoLessPlugin = require('craco-less');
+const path = require('path');
+
+const invade = (target, name, callback) => {
+    target.forEach((item) => {
+        if (item.constructor.name === name) {
+            callback(item);
+        }
+    });
+};
+
+const dropConsole = () => {
+    return (config) => {
+        if (process.env.NODE_ENV === 'production') {
+            invade(config.optimization.minimizer, 'TerserPlugin', (e) => {
+                e.options.extractComments = false;
+                e.options.terserOptions.compress.drop_console = true;
+            });
+        }
+        return config;
+    };
+};
+
+module.exports = {
+    webpack: {
+        alias: {
+            '@static': path.resolve(__dirname, 'src/static/'),
+        },
+        configure: (webpackConfig, {env, paths}) => {
+            webpackConfig.module.rules.push({
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: '@svgr/webpack',
+                        options: {
+                            babel: false,
+                            icon: true,
+                        },
+                    },
+                ],
+            });
+
+            webpackConfig = dropConsole()(webpackConfig);
+            return webpackConfig;
+        },
+    },
+    plugins: [
+        {
+            plugin: CracoLessPlugin,
+            options: {
+                lessLoaderOptions: {
+                    lessOptions: {
+                        modifyVars: {
+                            '@body-background': '#222734',
+                        },
+                        javascriptEnabled: true,
+                    },
+                },
+            },
+        },
+    ],
+    devServer: (devServerConfig, {proxy}) => ({
+        ...devServerConfig,
+        proxy: {
+            ...proxy,
+            '/api': {
+                target: 'http://106.38.203.236:8888/',
+                changeOrigin: true,
+                secure: false,
+                // pathRewrite: {
+                //     '/api': '/mock/71/api',
+                // },
+            },
+        },
+    }),
+    babel: {
+        presets: [],
+        plugins: [
+            ['import', {libraryName: 'antd', libraryDirectory: 'lib', style: true}, 'ant-design'],
+        ],
+        loaderOptions: (babelLoaderOptions, {env, paths}) => {
+            return babelLoaderOptions;
+        },
+    },
+};
